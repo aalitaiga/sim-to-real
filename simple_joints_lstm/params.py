3757966e1bcf1bff4d7da8b560fcd47d4776fcae
Florian Golemo <fgolemo@gmail.com>
2017-08-27 18:43:28 -0400

    added params for mb

--- simple_joints_lstm/params.py
+++ simple_joints_lstm/params.py
@@ -1,7 +1,7 @@
 HIDDEN_NODES = 128
 LSTM_LAYERS = 3
 CUDA = True
-EPOCHS = 20
+EPOCHS = 600
 DATASET_PATH = "./mujoco_data.h5"
-MODEL_PATH = "./trained_models/simple_lstm_128_op.pt"
-MODEL_PATH_BEST = "./trained_models/simple_lstm_128_op_best.pt"
+MODEL_PATH = "./trained_models/simple_lstm_128_mb.pt"
+MODEL_PATH_BEST = "./trained_models/simple_lstm_128_mb_best.pt"
