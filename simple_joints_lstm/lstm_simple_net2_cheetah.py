07d95c4069d590fee27f170370f21265a1ed9655
Florian Golemo <fgolemo@gmail.com>
2017-10-31 01:56:20 -0400

    added cheetah code

---
+++ simple_joints_lstm/lstm_simple_net2_cheetah.py
@@ -0,0 +1,44 @@
+import torch.nn.functional as F
+from torch import nn, autograd, torch
+
+from .params_cheetah import *
+
+
+class LstmSimpleNet2Cheetah(nn.Module):
+    def __init__(self):
+        super(LstmSimpleNet2Cheetah, self).__init__()
+
+        # because the LSTM is looking at 1 element at a time, and each element has 4 values
+        self.linear1 = nn.Linear(len(RELEVANT_COLUMNS), HIDDEN_NODES)
+        self.lstm1 = nn.LSTM(HIDDEN_NODES, HIDDEN_NODES, LSTM_LAYERS)
+        self.linear2 = nn.Linear(HIDDEN_NODES, len(RELEVANT_COLUMNS))
+
+        self.hidden = self.init_hidden()
+
+    def zero_hidden(self):
+        # print(dir(self.hidden[0]))
+        self.hidden[0].data.zero_()
+        self.hidden[1].data.zero_()
+
+    def init_hidden(self):
+        # the 1 here in the middle is the minibatch size
+        h, c = (autograd.Variable(torch.zeros(LSTM_LAYERS, 1, HIDDEN_NODES), requires_grad=False),
+                autograd.Variable(torch.zeros(LSTM_LAYERS, 1, HIDDEN_NODES), requires_grad=False))
+        if CUDA:
+            return h.cuda(), c.cuda()
+        return h, c
+
+    def forward(self, data_in):
+        # the view is to add the minibatch dimension (which is 1)
+        # print(data_in.view(1, 1, -1).size())
+        out = F.leaky_relu(self.linear1(data_in.view(1, -1)))
+        out, self.hidden = self.lstm1(out.view(1, 1, -1), self.hidden)
+        out = F.leaky_relu(out)
+        out = self.linear2(out.view(1, -1))
+
+        # out_pos = F.tanh(out[:,:2])
+        # out_vel = out[:,2:]
+        #
+        # out = torch.cat((out_pos, out_vel),dim=1)
+
+        return out
