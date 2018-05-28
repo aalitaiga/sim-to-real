d1a45bec4d654d93cfa05e59ad7a7e738fd4e1d0
Florian Golemo <fgolemo@gmail.com>
2018-05-28 16:45:31 -0300

    added new conversion scripts and first analysis for the new dataset

---
+++ real-robot/03-simulate-real-dataset.py
@@ -0,0 +1,43 @@
+import os
+
+from gym_ergojr.sim.single_robot import SingleRobot
+import time
+import numpy as np
+from tqdm import tqdm
+
+
+
+for run in ["train", "test"]:
+    robot = SingleRobot(debug=True, robot_model="ergojr-penholder")
+    ds = np.load(os.path.expanduser("~/data/sim2real/dataset-real-{}-normalized-onlyRobot.npz".format(run)))
+
+    ds_curr_real = ds["ds_curr_real"]
+    ds_next_real = ds["ds_next_real"]
+    ds_action = ds["ds_action"]
+    ds_epi = ds["ds_epi"]
+    ds_next_sim = []
+
+    print(ds_curr_real.shape)
+
+    for i in tqdm(range(len(ds_curr_real))):
+        robot.set(ds_curr_real[i])
+        # print(robot.observe().round(2))
+        robot.act2(ds_action[i])
+
+        robot.step()
+
+        obs = robot.observe()
+        ds_next_sim.append(obs)
+
+    ds_next_sim = np.array(ds_next_sim)
+
+    print(ds_next_sim.shape)
+
+    np.savez(os.path.expanduser("~/data/sim2real/dataset-real-{}-normalized-bullet.npz".format(run)),
+             ds_curr_real=ds_curr_real,
+             ds_next_real=ds_next_real,
+             ds_next_sim=ds_next_sim,
+             ds_action=ds_action,
+             ds_epi=ds_epi
+             )
+    robot.close()
