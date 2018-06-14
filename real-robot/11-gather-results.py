2adf4f7a4a9cdbabd07dc28501addb8f1e9a2e08
Florian Golemo <fgolemo@gmail.com>
2018-06-14 22:49:36 +0200

    aded real resultsø

--- real-robot/11-gather-results.py
+++ real-robot/11-gather-results.py
@@ -6,12 +6,11 @@ sim_file = [
     "../results/ErgoFightStatic-Headless-Shield-Move-HalfRand-Bullet-v0-180529135104-rewards-25-180530113054.npz",
     "../results/ErgoFightStatic-Headless-Shield-Move-HalfRand-Bullet-v0-180530211020-rewards-20-180614165501.npz"
 ]
-print ("sim")
 res = []
 for f in sim_file:
     data = np.load(f)["rewards"]
     res.append(np.mean(data))
-print (res)
+print("sim = {}".format(res))
 
 
 
@@ -19,12 +18,11 @@ real_file = [
     "../results/ErgoFight-Live-Shield-Move-HalfRand-v0-180508145449-rewards-25-180510233930.npz",
     "../results/ErgoFight-Live-Shield-Move-HalfRand-v0-180509120904.pt-rewards-25-180510-232208.npz"
 ]
-print ("real")
 res = []
 for f in real_file:
     data = np.load(f)["rewards"]
     res.append(np.mean(data))
-print(res)
+print("real = {}".format(res))
 
 
 
@@ -33,11 +31,10 @@ simplus_file = [
     "../results/ErgoFightStatic-Headless-Shield-Move-HalfRand-Bullet-Plus-Half-v0-180614160111-rewards-20-180614204335.npz"
 ]
 res = []
-print ("simplus")
 for f in simplus_file:
     data = np.load(f)["rewards"]
     res.append(np.mean(data))
-print(res)
+print("simplus = {}".format(res))
 
 
 
@@ -45,10 +42,9 @@ nosim_file = [
     "../results/ErgoFightStatic-Headless-Shield-Move-HalfRand-Bullet-NoSim-v0-180614184611-rewards-20-180614201131.npz"
 ]
 res = []
-print ("nosim")
 for f in nosim_file:
     data = np.load(f)["rewards"]
     res.append(np.mean(data))
-print(res)
+print("nosim = {}".format(res))
 
 
